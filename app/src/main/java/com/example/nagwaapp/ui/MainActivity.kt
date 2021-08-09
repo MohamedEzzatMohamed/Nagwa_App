efc2d0deaaa622c0e33992f6e1b8ebff3a8d6ade
MohamedEzzatMohamed <Mohamedezzat2552@gmail.com>
2021-08-09 10:52:18 +0200

    final version working without di

--- app/src/main/java/com/example/nagwaapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/nagwaapp/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nagwaapp.R
 
-//@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
